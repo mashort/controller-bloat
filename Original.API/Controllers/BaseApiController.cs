b1b2febefc4d51f78987901e806f39578f55b9e5
Martin Short <ma.short@yahoo.co.uk>
2021-11-15 16:28:48 +0000

    Add domain model and skeleton customer controller

---
+++ Original.API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Original.API.Controllers
+{
+    /// <summary>
+    /// Base API controller for common API behaviour
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+        public BaseApiController()
+        {
+        }
+    }
+}
---
+++ Original.API/DomainModels/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Original.API.DomainModels
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
