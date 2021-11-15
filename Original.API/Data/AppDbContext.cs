1e37ff33504c49fdb1914f69dcbe3c56665f1e73
mashort <ma.short@yahoo.co.uk>
2021-11-15 21:20:26 +0000

    Add EF Core and Customer entity type configuration

---
+++ Original.API/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Original.API.DomainModels;
+
+namespace Original.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ Original.API/Data/EntityTypeConfigurations/CustomerEntityTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Original.API.DomainModels;
+
+namespace Original.API.Data.EntityTypeConfigurations
+{
+    public class CustomerEntityTypeConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            //builder.ToTable();
+
+            builder.HasData(
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "",
+                    LastName = "",
+                    EmailAddress = ""
+                },
+                new Customer
+                {
+                    Id = 2,
+                    FirstName = "",
+                    LastName = "",
+                    EmailAddress = ""
+                });
+        }
+    }
+}
